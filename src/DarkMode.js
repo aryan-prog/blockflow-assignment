1f29bc86ffc55eb0659ab0adbeab500e3828c7dd
aryan-prog <mandelakalam12364@gmail.com>
2023-08-18 18:58:45 +0530

    dark mode enabled

--- src/DarkMode.js
+++ src/DarkMode.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import lr from './assets/rect-dark-right.png'
+import ll from './assets/rect-dark-left.png'
+import lightlogo from './assets/light-logo.png'
+import hexagon from './assets/hexagon.svg'
+import './LightMode.css'
+import sun from './assets/sun.png'
 import './DarkMode.css'
 
 
@@ -7,10 +13,33 @@ function DarkMode({ toggleDarkMode }) {
 
   return (
     <div className='Dark'>
-      <span>Dark Mode Component</span>
-      <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
-      {/* <p>Dark Mode: {isDarkMode ? 'On' : 'Off'}</p> */}
-    </div>
+    <nav className='navbar'>
+     <div className='blockflow-heading-dark'>
+       <img src={lightlogo} alt='logo'></img>
+       <span>blockflow</span> 
+     </div>
+     <div className='details-section-dark'>
+         <h3>DOCS</h3>
+         <h3>BLOG</h3>
+         <h3>PRICING</h3>
+         <h3>CONTACT US</h3>
+     </div>
+     <img src={sun} alt='switch modes' onClick={toggleDarkMode} id='sun'></img>
+   </nav>
+   <div className='middle-body'>
+     <div  className='banner-text'>
+     <div className='banner-dark'>Elevate your dapp development with blockflow</div>
+     <div className='sub-dark'>Efficient API Creation for Your Blockchain Data Needs.</div>
+     </div>
+     <div className='design-images'>
+       <img id='hex1'src={hexagon} alt='diamond'></img>
+       <img id='dark1'src={ll} alt='diamond'></img>
+       <img id='dark2'src={lr} alt='diamond'></img>
+       <img id='hex2'src={hexagon} alt='diamond'></img>
+     </div>
+   </div>
+   <button className='request-demo-dark'>Request Demo</button>
+ </div>
   );
 }
 
